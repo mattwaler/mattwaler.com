8d4eeb6f250076e1c2deef308961c2322027cc67
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2021-06-29 13:22:20 -0400

    Adding avatar, switching offering order

--- src/_data/offerings.js
+++ src/_data/offerings.js
@@ -3,17 +3,32 @@ module.exports = [
     heading: 'The Big Names',
     description:
       'The stuff we constantly hear about. These are the skills that can get you hired anywhere.',
-    bullets: ['React', 'Node', 'Svelte', 'Vue'],
+    bullets: [
+      'Node',
+      'React',
+      'Svelte',
+      'Vue'
+    ],
   },
   {
     heading: 'The Lesser Knowns',
     description:
       'Tools you do not hear about nearly as much, but pack just as much punch, possibly even more so.',
-    bullets: ['Tailwind CSS', 'AlpineJS', 'Eleventy', 'Craft CMS'],
+    bullets: [
+      'AlpineJS',
+      'Craft CMS',
+      'Eleventy',
+      'Tailwind CSS',
+    ],
   },
   {
     heading: 'Tooling',
     description: 'These are the tools that will up your game across the board.',
-    bullets: ['NPM', 'VS Code', 'ZSH', 'Git'],
+    bullets: [
+      'Git',
+      'NPM',
+      'VS Code',
+      'ZSH',
+    ],
   },
 ]
