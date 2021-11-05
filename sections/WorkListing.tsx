5743ba805d90e4296a8b1e872b2af4ce65bfe170
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2021-11-04 23:19:03 -0400

    Adding sites, tools, and better images to work section.

--- sections/WorkListing.tsx
+++ sections/WorkListing.tsx
@@ -20,8 +20,18 @@ export default function WorkListing() {
               <p className={`text-sm font-medium underline ${item.color}`}>{item.link}</p>
             </a>
             <p className="text-base text-gray-700 mt-4">{item.description}</p>
+            <ul className="flex items-center gap-1 mt-4">
+              {item.tools.map(tool => (
+                <li
+                  key={tool}
+                  className="text-xs tracking-tight font-bold uppercase bg-gray-300 text-gray-800 px-3 py-1 rounded-2xl"
+                >
+                  {tool}
+                </li>
+              ))}
+            </ul>
             <a target="_blank" href={item.link} className={`inline-flex items-center mt-6 ${item.color} font-bold`}>
-              <span className="leading-none">View Project</span>
+              <span className="leading-none">Visit {item.title}</span>
               <span className="w-4 ml-1">
                 <ArrowRightIcon />
               </span>
