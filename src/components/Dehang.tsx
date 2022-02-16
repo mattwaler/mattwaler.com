bb0563f30dbb9765f3732f910893592851e5d213
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2022-02-15 20:42:24 -0500

    Fix dehang component with really long strings

--- src/components/Dehang.tsx
+++ src/components/Dehang.tsx
@@ -9,13 +9,25 @@ interface Props {
 export default function Dehang(props: Props) {
   const { className, tag, text } = props
 
+  // Reconstruct string with break tag
   const array = text.split(' ')
+  const length = array.length
   const withoutLastTwo = array.slice(0, -2)
-  const lastTwoString = `${array[array.length - 2]}&nbsp;${
-    array[array.length - 1]
-  }`
+  const lastTwoString = `${array[length - 2]}&nbsp;${array[length - 1]}`
   const newArray = [...withoutLastTwo, lastTwoString]
   const newString = newArray.join(' ')
 
-  return createElement(tag ?? 'span', { className, dangerouslySetInnerHTML: { __html: newString } })
+  // Bail if last string is too long
+  if (lastTwoString.length > 24) {
+    return createElement(tag ?? 'span', {
+      className,
+      dangerouslySetInnerHTML: { __html: text },
+    })
+  }
+
+  // Return dehung string if short enough
+  return createElement(tag ?? 'span', {
+    className,
+    dangerouslySetInnerHTML: { __html: newString },
+  })
 }
