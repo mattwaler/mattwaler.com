c5af0330fbdcaf180aa8e770f1934ed8b5b2ac0b
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2023-06-30 15:24:53 -0400

    Fixing sitemap homepage URL

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 export default async function sitemap() {
-  const slugs = ['/', '/contact', '/instruction', '/work']
+  const slugs = ['', '/contact', '/instruction', '/work']
   const pages = slugs.map((page) => ({
     url: `https://mattwaler.com${page}`,
     lastModified: new Date().toISOString(),
