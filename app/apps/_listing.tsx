404f2c1b086bf4cdf0b57d0c1536ea7c63cb4b50
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2024-09-12 11:52:55 -0400

    Updating app names

--- app/apps/_listing.tsx
+++ app/apps/_listing.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 
 const content = [
   {
-    heading: 'Agone',
+    heading: 'Agone: Task Tracker',
     description:
       'Agone is a recurring task tracker. You can create repeating items, custom groups, and get completion history and push notifications when things are due.',
     link: '/apps/agone',
@@ -15,7 +15,7 @@ const content = [
     tools: ['Swift', 'Swift UI', 'Swift Data'],
   },
   {
-    heading: 'Fitness Timer',
+    heading: 'Oft: Fitness Timer',
     description:
       'A no-fuss fitness timer that allows a user to customize timer duration, repeats, and rest time.',
     link: 'https://apps.apple.com/us/app/fitness-timer-simple/id1659047701?platform=iphone',
