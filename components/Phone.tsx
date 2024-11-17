2d22928391cab2c449ad0fea4d9b3715e50e4cbe
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2024-11-17 12:57:14 -0500

    Better phone component responsiveness

--- components/Phone.tsx
+++ components/Phone.tsx
@@ -1,17 +1,14 @@
 export default function Phone(props) {
   return (
-    <div className="relative aspect-[288/600] rounded-[45px] shadow-[0_0_2px_2px_rgba(255,255,255,0.1)] border-8 border-zinc-900 ml-1">
-      {props.notch && (
-        <div className="absolute top-2 left-1/2 transform -translate-x-1/2 w-[90px] h-[22px] bg-zinc-900 rounded-full z-20"></div>
-      )}
-      <div className="absolute -inset-[1px] border-[3px] border-zinc-700 border-opacity-40 rounded-[37px] pointer-events-none"></div>
-      <div className="relative w-full h-full rounded-[37px] overflow-hidden flex items-center justify-center  bg-zinc-900/10">
+    <div className="relative aspect-[288/600] rounded-3xl lg:rounded-[45px] shadow-[0_0_2px_2px_rgba(255,255,255,0.1)] border-8 border-zinc-900 ml-1">
+      <div className="absolute -inset-[1px] rounded-2xl border-[3px] border-zinc-700 border-opacity-40 lg:rounded-[37px] pointer-events-none"></div>
+      <div className="relative w-full h-full rounded-2xl lg:rounded-[37px] overflow-hidden flex items-center justify-center  bg-zinc-900/10">
         {props.children}
       </div>
-      <div className="absolute left-[-12px] top-20 w-[6px] h-8 bg-zinc-900 rounded-l-md shadow-md"></div>
-      <div className="absolute left-[-12px] top-36 w-[6px] h-12 bg-zinc-900 rounded-l-md shadow-md"></div>
-      <div className="absolute left-[-12px] top-52 w-[6px] h-12 bg-zinc-900 rounded-l-md shadow-md"></div>
-      <div className="absolute right-[-12px] top-36 w-[6px] h-16 bg-zinc-900 rounded-r-md shadow-md"></div>
+      <div className="hidden sm:block absolute left-[-12px] top-20 w-[6px] h-8 bg-zinc-900 rounded-l-md shadow-md"></div>
+      <div className="hidden sm:block absolute left-[-12px] top-36 w-[6px] h-12 bg-zinc-900 rounded-l-md shadow-md"></div>
+      <div className="hidden sm:block absolute left-[-12px] top-52 w-[6px] h-12 bg-zinc-900 rounded-l-md shadow-md"></div>
+      <div className="hidden sm:block absolute right-[-12px] top-36 w-[6px] h-16 bg-zinc-900 rounded-r-md shadow-md"></div>
     </div>
   )
 }
