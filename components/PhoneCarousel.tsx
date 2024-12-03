f8b6b122125bd2dcc544c610c6de47935bf1136d
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2024-12-03 09:45:30 -0500

    A11y changes to carousels

--- components/PhoneCarousel.tsx
+++ components/PhoneCarousel.tsx
@@ -28,12 +28,13 @@ export default function PhoneCarousel(props) {
 
   return (
     <div className="flex items-center gap-4">
-      <button onClick={previous} className={props.color}>
+      <button aria-label="View previous image" onClick={previous} className={props.color}>
         <ArrowLeftCircleIcon className="size-12" />
       </button>
       <div className="relative aspect-[288/600] rounded-3xl lg:rounded-[45px] shadow-[0_0_2px_2px_rgba(255,255,255,0.1)] border-8 border-zinc-900 ml-1">
         <div className="absolute -inset-[1px] rounded-2xl border-[3px] border-zinc-700 border-opacity-40 lg:rounded-[37px] pointer-events-none"></div>
-        <div className="relative w-full h-full rounded-2xl lg:rounded-[37px] overflow-hidden bg-zinc-900/10 grid grid-cols-1 grid-rows-1">
+        <div className="aspect-[1206/2622] relative w-full h-full rounded-2xl lg:rounded-[37px] overflow-hidden bg-zinc-900/10 grid grid-cols-1 grid-rows-1">
+          <div className="z-[-1] col-start-1 row-start-1 bg-white"></div>
           {props.images.map((image, index) => (
             <Image
               key={index}
@@ -42,7 +43,6 @@ export default function PhoneCarousel(props) {
               height={2622}
               style={{ zIndex: index }}
               className={`col-start-1 row-start-1 size-full transition-opacity duration-500 ${activeIndex === index ? 'opacity-1' : 'opacity-0'}`}
-              loading="lazy"
               placeholder="blur"
               src={image}
             />
@@ -53,7 +53,7 @@ export default function PhoneCarousel(props) {
         <div className="hidden sm:block absolute left-[-12px] top-52 w-[6px] h-12 bg-zinc-900 rounded-l-md shadow-md"></div>
         <div className="hidden sm:block absolute right-[-12px] top-36 w-[6px] h-16 bg-zinc-900 rounded-r-md shadow-md"></div>
       </div>
-      <button onClick={next} className={props.color}>
+      <button aria-label="View next image" onClick={next} className={props.color}>
         <ArrowLeftCircleIcon className="size-12 rotate-180" />
       </button>
     </div>
