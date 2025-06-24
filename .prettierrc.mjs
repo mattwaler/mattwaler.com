324b26141f56d1ea4301491d9649661faa439929
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2025-06-24 14:27:04 -0400

    Formatting

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -6,5 +6,5 @@ export default {
   tabWidth: 2,
   semi: false,
   singleQuote: true,
-  plugins: ['prettier-plugin-astro'],
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
 };
