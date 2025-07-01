ac532a0f3879924c55373126b04ade7ced903956
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2025-07-01 15:32:20 -0400

    Better projects view

--- src/assets/projects.ts
+++ src/assets/projects.ts
@@ -4,15 +4,27 @@ export default [
     description:
       'A fully static marketing website for a local landscaping company that provides lots of different services.',
     link: 'https://kingscourtlandscaping.com',
-    color: 'text-[#ecb742]',
     tools: ['Astro', 'Tailwind', 'Alpine'],
+  },
+  {
+    title: 'Agone: Task Tracker App',
+    description:
+      'Agone is a recurring task tracker. You can create repeating items, custom groups, and get completion history and push notifications when things are due.',
+    link: '/projects/agone/',
+    tools: ['Swift', 'Swift UI', 'Swift Data'],
+  },
+  {
+    title: 'Oft: Fitness Timer App',
+    description:
+      'A no-fuss fitness timer that allows a user to customize timer duration, repeats, and rest time.',
+    link: 'https://apps.apple.com/us/app/fitness-timer-simple/id1659047701?platform=iphone',
+    tools: ['Swift', 'Swift UI'],
   },
   {
     title: 'Rev Indy',
     description:
       'A modular marketing website to bring awareness to the Rev Indy fundraising event that takes place every year.',
     link: 'https://revindy.org/',
-    color: 'text-[#bdcb81]',
     tools: ['Craft', 'Tailwind', 'Alpine'],
   },
   {
@@ -20,7 +32,6 @@ export default [
     description:
       'An Indiana-based marketing agency focused on delivering digital experiences to clients that improve lives.',
     link: 'https://trendyminds.com',
-    color: 'text-[#f79024]',
     tools: ['Craft', 'Tailwind', 'Alpine'],
   },
   {
@@ -28,7 +39,6 @@ export default [
     description:
       'A CMS built for a group of public schools that teaches academic content and life lessons through a structured, high-quality curriculum.',
     link: 'https://paramountindy.org/',
-    color: 'text-[#64bd62]',
     tools: ['Craft', 'Tailwind', 'Alpine'],
   },
   {
@@ -36,7 +46,6 @@ export default [
     description:
       'A monolithic Craft CMS website for an insurance, employee benefits and risk management consulting company.',
     link: 'https://hylant.com/',
-    color: 'text-[#6bbbae]',
     tools: ['Craft CMS', 'Tailwind', 'Alpine', 'Sprig'],
   },
   {
@@ -44,7 +53,6 @@ export default [
     description:
       'A website that aims to help individuals find meaningful jobs or internships in the greater Indianapolis area.',
     link: 'https://ascendindiana.com/',
-    color: 'text-[#c95717]',
     tools: ['Craft', 'Tailwind', 'Alpine'],
   },
   {
@@ -52,7 +60,6 @@ export default [
     description:
       'A research-aggregate website focused on providing tools for people to promote youth development.',
     link: 'https://searchinstitute.org',
-    color: 'text-[#d20e2b]',
     tools: ['Craft', 'Tailwind', 'Alpine', 'React', 'Algolia'],
   },
   {
@@ -60,7 +67,6 @@ export default [
     description:
       'A website built to offer information for families in their search for high quality learning experiences for their children.',
     link: 'https://brighterfuturesindiana.org',
-    color: 'text-[#4dc5cd]',
     tools: ['Craft', 'Tailwind', 'Alpine'],
   },
   {
@@ -68,7 +74,6 @@ export default [
     description:
       'A web tool to help business owners calculate both supplies and the maximum number of occupants they can allow.',
     link: 'https://covid.trendyminds.com',
-    color: 'text-[#4c8fbc]',
     tools: ['Eleventy', 'Tailwind', 'Svelte'],
   },
   {
@@ -76,7 +81,6 @@ export default [
     description:
       'A website highlighting Hamilton Exhibits innovative brand spaces and how they can make an impact.',
     link: 'https://www.hamilton-exhibits.com/',
-    color: 'text-[#0099cc]',
     tools: ['Craft', 'React', 'Gatsby'],
   },
   {
@@ -84,7 +88,6 @@ export default [
     description:
       'A nation-wide website to communicate and highlight the first Greek-letter fraternity for women.',
     link: 'https://kappaalphatheta.org',
-    color: 'text-[#d8b511]',
     tools: ['Craft', 'React'],
   },
   {
@@ -92,7 +95,6 @@ export default [
     description:
       'A full-service web application for the Indiana branch of University Hospitals.',
     link: 'https://iuhealth.org',
-    color: 'text-[#1a7e9f]',
     tools: ['Craft', 'Tailwind', 'Alpine', 'React', 'Algolia'],
   },
   {
@@ -100,7 +102,6 @@ export default [
     description:
       'A monolithic web application for the Pediatric branches of University Hospitals in Indiana.',
     link: 'https://rileychildrens.org',
-    color: 'text-[#f0b921]',
     tools: ['Craft', 'Tailwind', 'Alpine', 'React', 'Algolia'],
   },
   {
@@ -108,7 +109,6 @@ export default [
     description:
       'A marketing website for a Cleveland-based full-service semi-trailer dealership.',
     link: 'https://www.transportservices.com/',
-    color: 'text-[#f9edbe]',
     tools: ['Kentico', 'Sass', 'Pug'],
   },
   {
@@ -116,7 +116,6 @@ export default [
     description:
       'An e-commerce website for a Cleveland-based nature-first juice company.',
     link: 'https://gardenofflavor.com',
-    color: 'text-[#00a950]',
     tools: ['Shopify'],
   },
 ]
