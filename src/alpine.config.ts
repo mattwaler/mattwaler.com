71154b8cfe71e94d0a5c45bdbed748c17ab86cd3
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2025-11-30 15:38:33 -0500

    Adding typing effect

--- src/alpine.config.ts
+++ src/alpine.config.ts
@@ -1,5 +1,36 @@
 import type { Alpine } from 'alpinejs'
+import Typed from 'typed.js'
 
 export default (Alpine: Alpine) => {
   Alpine.store('isNavOpen', false)
 }
+
+// Add Typing Animation
+const typedDivs = document.querySelectorAll('[data-typed]')
+typedDivs.forEach(div => {
+  const id = div.getAttribute('data-typed')
+  if (!sessionStorage.getItem(id)) {
+    // Create New Element
+    const typedDiv = document.createElement('div')
+    // Copy all attributes over
+    const attr = div.attributes
+    for (let i = 0; i < attr.length; i++) {
+      const attribute = attr[i];
+      typedDiv.setAttribute(attribute.name, attribute.value);
+    }
+    // Remove data attribute hiding new div
+    typedDiv.removeAttribute('data-typed')
+    // Insert New Div & Type!
+    div.parentNode.insertBefore(typedDiv, div.nextSibling)
+    div.classList.add('sr-only')
+    new Typed(typedDiv, {
+      strings: [div.innerHTML],
+      backSpeed: 0,
+      backDelay: 0,
+      loop: false,
+    })
+    sessionStorage.setItem(id, 'DONE')
+  } else {
+    div.removeAttribute('data-typed')
+  }
+})
