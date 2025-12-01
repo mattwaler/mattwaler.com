4cb918c7d0d18ce7c33ae2d4ac072d82af260b3f
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2025-12-01 13:15:27 -0500

    Whoops remove true

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ typedDivs.forEach(div => {
   const id = div.getAttribute('data-typed')
   const reduced = window.matchMedia("(prefers-reduced-motion: reduce)")
 
-  if (!reduced.matches && !sessionStorage.getItem(id) || true) {
+  if (!reduced.matches && !sessionStorage.getItem(id)) {
     // Get div attributes
     const attr = div.attributes
 
