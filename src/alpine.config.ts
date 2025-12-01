625a0699be53819ad0364b79551fd79fae7cf314
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2025-12-01 10:59:49 -0500

    Fixing typing effect

--- src/alpine.config.ts
+++ src/alpine.config.ts
@@ -8,27 +8,51 @@ export default (Alpine: Alpine) => {
 // Add Typing Animation
 const typedDivs = document.querySelectorAll('[data-typed]')
 typedDivs.forEach(div => {
+  div.classList.add('relative')
   const id = div.getAttribute('data-typed')
-  if (!sessionStorage.getItem(id)) {
-    // Create New Element
-    const typedDiv = document.createElement('div')
-    // Copy all attributes over
+  if (!sessionStorage.getItem(id) || import.meta.env.DEV) {
     const attr = div.attributes
+
+    // Create Clickable Div
+    const clickableDiv = document.createElement('div')
+    clickableDiv.innerHTML = div.innerHTML
+    for (let i = 0; i < attr.length; i++) {
+      const attribute = attr[i];
+      clickableDiv.setAttribute(attribute.name, attribute.value);
+    }
+    clickableDiv.setAttribute('data-clickable', '')
+    clickableDiv.removeAttribute('data-typed')
+    clickableDiv.classList.add('opacity-0', 'relative', 'z-[1]')
+
+    // Create Typing Div
+    const typedDiv = document.createElement('div')
     for (let i = 0; i < attr.length; i++) {
       const attribute = attr[i];
       typedDiv.setAttribute(attribute.name, attribute.value);
     }
-    // Remove data attribute hiding new div
+    typedDiv.setAttribute('data-typing', '')
     typedDiv.removeAttribute('data-typed')
-    // Insert New Div & Type!
-    div.parentNode.insertBefore(typedDiv, div.nextSibling)
+    typedDiv.classList.add('absolute!', 'inset-0!', 'size-full!', 'z-[2]!', 'pointer-events-none!')
+
+    // Wipe Div & Create New Structure
+    div.innerHTML = ""
+    div.appendChild(clickableDiv)
+    div.appendChild(typedDiv)
+    div.setAttribute('class', 'relative')
+    div.removeAttribute('data-typed')
+
+    // Type New Div!
     new Typed(typedDiv, {
-      strings: [div.innerHTML],
+      strings: [clickableDiv.innerHTML],
       showCursor: false,
       contentType: 'html',
       backSpeed: 0,
       backDelay: 0,
       loop: false,
+      onComplete() {
+        typedDiv.classList.add('hidden!')
+        clickableDiv.classList.remove('opacity-0')
+      }
     })
     sessionStorage.setItem(id, 'DONE')
   } else {
