82e0657615f020e59965045b37937680f37458e7
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2025-12-23 11:35:49 -0500

    View transitions??

--- src/_bundle/app.js
+++ src/_bundle/app.js
@@ -1,8 +1,10 @@
 import Alpine from 'alpinejs'
 import Typed from 'typed.js'
-import { intersect } from '@alpinejs/intersect';
-import { Masonry } from 'grid-rows-masonry';
+import { intersect } from '@alpinejs/intersect'
+import { anchor } from '@alpinejs/anchor'
+import { Masonry } from 'grid-rows-masonry'
 
+Alpine.plugin(anchor)
 Alpine.plugin(intersect)
 
 window.Alpine = Alpine
