b528ad077977136296d043a260a78ec64a2f7449
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2026-09-04 16:37:23 -0400

    Mass updates

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -1,5 +1,6 @@
 export default [
-  { name: 'Contact', route: '/contact/' },
+  { name: 'Web Development', route: '/freelance-web-development/' },
+  { name: 'App Development', route: '/freelance-mobile-application-development/' },
   { name: 'Projects', route: '/projects/' },
   { name: 'Blog', route: '/blog/' },
 ]
