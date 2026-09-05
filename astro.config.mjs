9ffda60fabd1fab12daac911cf01dee37fe35c59
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2026-09-05 10:01:35 -0400

    Adding schema, removing personal pages from sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,20 @@ import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 import sitemap from '@astrojs/sitemap'
 
+const excludedPages = [
+  'https://mattwaler.com/dad/',
+  'https://mattwaler.com/klaus/',
+];
+
 export default defineConfig({
   site: 'https://mattwaler.com',
   trailingSlash: 'always',
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [sitemap()],
+  integrations: [
+    sitemap({
+      filter: (page) => !excludedPages.includes(page),
+    }),
+  ],
 })
