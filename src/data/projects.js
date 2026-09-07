48fe92b3403e6a9ecafc9ef34dcc3cde002bb7f3
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2026-09-07 09:49:40 -0400

    Adjusting latest project order

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,135 +1,135 @@
 export default [
-    {
-      title: 'Hamilton Exhibits',
-      description:
-        'A website highlighting Hamilton Exhibits innovative brand spaces and how they can make an impact.',
-      link: 'https://hamilton-ex.com/',
-      tools: ['Statamic', 'Laravel', 'Tailwind', 'Alpine'],
-      trendy: true,
-    },
-    {
-      title: 'TrendyMinds',
-      description:
-        'An Indiana-based marketing agency focused on delivering digital experiences to clients that improve lives.',
-      link: 'https://trendyminds.com',
-      tools: ['Statamic', 'Laravel', 'Tailwind', 'Alpine'],
-      trendy: true,
-  },
   {
-    title: 'Cakewalk Development',
+    title: "TrendyMinds",
     description:
-      'Cakewalk Development is a two-man development agency that aims to make web development easier for individuals and small businesses that have a lot on their plates.',
-    link: 'https://cakewalkdev.com',
-    tools: ['Eleventy', 'Tailwind', 'Alpine'],
-  },
-  {
-    title: 'Repeaters',
-    description:
-      'Repeaters is a recurring task tracker. You can create repeating items, custom groups, and get completion history and push notifications when things are due.',
-    link: 'https://repeaters.mattwaler.com',
-    tools: ['Swift'],
-  },
-  {
-    title: 'Kings Court Landscaping',
-    description:
-      'A fully static marketing website for a local landscaping company that provides lots of different services.',
-    link: 'https://kingscourtlandscaping.com',
-    tools: ['Astro', 'Tailwind', 'Alpine'],
-  },
-  {
-    title: 'Rev Indy',
-    description:
-      'A modular marketing website to bring awareness to the Rev Indy fundraising event that takes place every year.',
-    link: 'https://revindy.org/',
-    tools: ['Craft', 'Tailwind', 'Alpine'],
+      "An Indiana-based marketing agency focused on delivering digital experiences to clients that improve lives.",
+    link: "https://trendyminds.com",
+    tools: ["Statamic", "Laravel", "Tailwind", "Alpine"],
     trendy: true,
   },
   {
-    title: 'Intervals',
+    title: "Repeaters",
     description:
-      'A no-fuss fitness timer that allows a user to customize timer duration, repeats, and rest time.',
-    link: 'https://apps.apple.com/us/app/fitness-timer-simple/id1659047701?platform=iphone',
-    tools: ['Swift'],
+      "Repeaters is a recurring task tracker. You can create repeating items, custom groups, and get completion history and push notifications when things are due.",
+    link: "https://repeaters.mattwaler.com",
+    tools: ["Swift"],
+  },
+  {
+    title: "Kings Court Landscaping",
+    description:
+      "A fully static marketing website for a local landscaping company that provides lots of different services.",
+    link: "https://kingscourtlandscaping.com",
+    tools: ["Astro", "Tailwind", "Alpine"],
+  },
+  {
+    title: "Hamilton Exhibits",
+    description:
+      "A website highlighting Hamilton Exhibits innovative brand spaces and how they can make an impact.",
+    link: "https://hamilton-ex.com/",
+    tools: ["Statamic", "Laravel", "Tailwind", "Alpine"],
+    trendy: true,
+  },
+  {
+    title: "Cakewalk Development",
+    description:
+      "Cakewalk Development is a two-man development agency that aims to make web development easier for individuals and small businesses that have a lot on their plates.",
+    link: "https://cakewalkdev.com",
+    tools: ["Eleventy", "Tailwind", "Alpine"],
+  },
+  {
+    title: "Rev Indy",
+    description:
+      "A modular marketing website to bring awareness to the Rev Indy fundraising event that takes place every year.",
+    link: "https://revindy.org/",
+    tools: ["Craft", "Tailwind", "Alpine"],
+    trendy: true,
+  },
+  {
+    title: "Intervals",
+    description:
+      "A no-fuss fitness timer that allows a user to customize timer duration, repeats, and rest time.",
+    link: "https://apps.apple.com/us/app/fitness-timer-simple/id1659047701?platform=iphone",
+    tools: ["Swift"],
   },
 
   {
-    title: 'Paramount Schools of Excellence',
+    title: "Paramount Schools of Excellence",
     description:
-      'A CMS built for a group of public schools that teaches academic content and life lessons through a structured, high-quality curriculum.',
-    link: 'https://paramountindy.org/',
-    tools: ['Craft', 'Tailwind', 'Alpine'],
+      "A CMS built for a group of public schools that teaches academic content and life lessons through a structured, high-quality curriculum.",
+    link: "https://paramountindy.org/",
+    tools: ["Craft", "Tailwind", "Alpine"],
     trendy: true,
   },
   {
-    title: 'Hylant',
+    title: "Hylant",
     description:
-      'A monolithic Craft CMS website for an insurance, employee benefits and risk management consulting company.',
-    link: 'https://hylant.com/',
-    tools: ['Craft', 'Tailwind', 'Alpine'],
+      "A monolithic Craft CMS website for an insurance, employee benefits and risk management consulting company.",
+    link: "https://hylant.com/",
+    tools: ["Craft", "Tailwind", "Alpine"],
     trendy: true,
   },
   {
-    title: 'Ascend Indiana',
+    title: "Ascend Indiana",
     description:
-      'A website that aims to help individuals find meaningful jobs or internships in the greater Indianapolis area.',
-    link: 'https://ascendindiana.com/',
-    tools: ['Craft', 'Tailwind', 'Alpine'],
+      "A website that aims to help individuals find meaningful jobs or internships in the greater Indianapolis area.",
+    link: "https://ascendindiana.com/",
+    tools: ["Craft", "Tailwind", "Alpine"],
     trendy: true,
   },
   {
-    title: 'Search Institute',
+    title: "Search Institute",
     description:
-      'A research-aggregate website focused on providing tools for people to promote youth development.',
-    link: 'https://searchinstitute.org',
-    tools: ['Craft', 'Tailwind', 'Alpine', 'React', 'Algolia'],
+      "A research-aggregate website focused on providing tools for people to promote youth development.",
+    link: "https://searchinstitute.org",
+    tools: ["Craft", "Tailwind", "Alpine", "React", "Algolia"],
     trendy: true,
   },
   {
-    title: 'Brighter Futures Indiana',
+    title: "Brighter Futures Indiana",
     description:
-      'A website built to offer information for families in their search for high quality learning experiences for their children.',
-    link: 'https://brighterfuturesindiana.org',
-    tools: ['Craft', 'Tailwind', 'Alpine'],
+      "A website built to offer information for families in their search for high quality learning experiences for their children.",
+    link: "https://brighterfuturesindiana.org",
+    tools: ["Craft", "Tailwind", "Alpine"],
     trendy: true,
   },
   {
-    title: 'Covid Resources',
+    title: "Covid Resources",
     description:
-      'A web tool to help business owners calculate both supplies and the maximum number of occupants they can allow.',
-    link: 'https://covid.trendyminds.com',
-    tools: ['Eleventy', 'Tailwind'],
+      "A web tool to help business owners calculate both supplies and the maximum number of occupants they can allow.",
+    link: "https://covid.trendyminds.com",
+    tools: ["Eleventy", "Tailwind"],
     trendy: true,
   },
 
   {
-    title: 'Kappa Alpha Theta',
+    title: "Kappa Alpha Theta",
     description:
-      'A nation-wide website to communicate and highlight the first Greek-letter fraternity for women.',
-    link: 'https://kappaalphatheta.org',
-    tools: ['Craft', 'React'],
+      "A nation-wide website to communicate and highlight the first Greek-letter fraternity for women.",
+    link: "https://kappaalphatheta.org",
+    tools: ["Craft", "React"],
     trendy: true,
   },
   {
-    title: 'IU Health',
+    title: "IU Health",
     description:
-      'A full-service web application for the Indiana branch of University Hospitals.',
-    link: 'https://iuhealth.org',
-    tools: ['Craft', 'Tailwind', 'Alpine', 'React', 'Algolia'],
+      "A full-service web application for the Indiana branch of University Hospitals.",
+    link: "https://iuhealth.org",
+    tools: ["Craft", "Tailwind", "Alpine", "React", "Algolia"],
     trendy: true,
   },
   {
-    title: 'Riley Childrens',
+    title: "Riley Childrens",
     description:
-      'A monolithic web application for the Pediatric branches of University Hospitals in Indiana.',
-    link: 'https://rileychildrens.org',
-    tools: ['Craft', 'Tailwind', 'Alpine', 'React', 'Algolia'],
+      "A monolithic web application for the Pediatric branches of University Hospitals in Indiana.",
+    link: "https://rileychildrens.org",
+    tools: ["Craft", "Tailwind", "Alpine", "React", "Algolia"],
     trendy: true,
   },
   {
-    title: 'Garden of Flavor',
+    title: "Garden of Flavor",
     description:
-      'An e-commerce website for a Cleveland-based nature-first juice company.',
-    link: 'https://gardenofflavor.com',
-    tools: ['Shopify', 'Tailwind'],
+      "An e-commerce website for a Cleveland-based nature-first juice company.",
+    link: "https://gardenofflavor.com",
+    tools: ["Shopify", "Tailwind"],
   },
-]
+];
