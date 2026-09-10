16237c36a0a254c4504033ec91526b33cc60ebc4
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2026-09-10 13:58:43 -0400

    Better nav structure

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -1,6 +1,9 @@
-export default [
-  { name: 'Web Development', route: '/freelance-web-development' },
-  { name: 'App Development', route: '/freelance-mobile-application-development' },
-  { name: 'Projects', route: '/projects' },
+export const nav1 = [
   { name: 'Blog', route: '/blog' },
+  { name: 'Projects', route: '/projects' },
+]
+
+export const nav2 = [
+  { name: 'App Development', route: '/freelance-mobile-application-development' },
+  { name: 'Web Development', route: '/freelance-web-development' },
 ]
